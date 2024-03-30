1773005e989261250206cbd78c9499236d7974ab
Dan E <dhengland44@gmail.com>
2024-03-30 23:44:09 +0000

    Added a metal detector for Iron Ore and Diamond Ore

--- src/main/java/net/danthepythonman/tutorialmod/item/ModItems.java
+++ src/main/java/net/danthepythonman/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.danthepythonman.tutorialmod.item;
 
 import net.danthepythonman.tutorialmod.TutorialMod;
+import net.danthepythonman.tutorialmod.item.custom.MetalDetectorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -16,6 +17,7 @@ public class ModItems {
 
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+    public static final Item METAL_DETECTOR = registerItem("metal_detector", new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
 
     private  static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries){
         entries.add(RUBY);
